334b6fe72da4ef3d5350db8d786925bb61cf3289
Nando <fernandocalva15@gmail.com>
2026-06-24 01:21:23 -0600

    Agregar templates como base.html, para mejorar fluides y cambios a views.py para declarar las funciones correspondientes(flujo basico de inicio de sesion, registro y cierre de sesion completado), pendiente urls

--- task/views.py
+++ task/views.py
@@ -1,3 +1,61 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.contrib.auth import login, logout, authenticate
+from django.http import HttpResponse
+from django.db import IntegrityError
 
 # Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def signup(request):
+    if request.method == 'GET':
+        print("Enviando formulario")
+        return render(request, 'signup.html', {
+            'form': UserCreationForm()
+        })
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            print("Contraseñas coinciden")
+            #Registrando usuario
+            try:
+                user = User.objects.create_user(username=request.POST['username'], password=request.POST['password1'])
+                user.save()
+                login(request, user)
+                return redirect('task')
+            except IntegrityError:
+                return render(request, 'signup.html', {
+                    'form': UserCreationForm(),
+                    "error": "Usuario ya existe"
+                })
+            
+        else:
+            return render(request, 'signup.html', {
+                'form': UserCreationForm(),
+                "error": "Contraseñas no coinciden"
+            })
+        
+def task(request):
+    return render(request, 'task.html')
+
+def signout(request):
+    logout(request)
+    return redirect('home')
+
+def signin(request):
+    if request.method == 'GET':
+        return render(request, 'signin.html')
+    else:
+        user = authenticate(
+            request,
+            username=request.POST['username'],
+            password=request.POST['password']
+        )
+        if user is None:
+            return render(request, 'signin.html', {
+                "error": "Nombre de usuario o contraseña incorrectos"
+            })
+        else:
+            login(request, user)
+            return redirect('task')
