a16baf7eebb977e021737652a1e79cd727399f00
Nando <fernandocalva15@gmail.com>
2026-06-24 03:07:00 -0600

    se instalo app task, se agregaron las urls e importacion de vistas de app task

--- djangocrud/urls.py
+++ djangocrud/urls.py
@@ -16,7 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from task import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    path('signup/', views.signup, name='signup'),
+    path('task/', views.task, name='task'),
+    path('logout/', views.signout, name='signout'),
+    path('signin/', views.signin, name='signin'),
 ]
