88c2587af1b29077a0085f8196b933d72906e9a2
Nando <fernandocalva15@gmail.com>
2026-06-26 22:53:37 -0600

    Modelo y migracion de columnas para Tarea

--- task/models.py
+++ task/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Task(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    date_completed = models.DateTimeField(null=True, blank=True)
+    date_created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    priority = models.IntegerField(default=0)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.title + ' - by ' + self.user.username
