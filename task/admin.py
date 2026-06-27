7743efee24723d1d3f57dd2533e32946a69302cc
Nando <fernandocalva15@gmail.com>
2026-06-26 23:20:27 -0600

    cambio basico a la vista de admin al ver las tareas, template, url y vista basica para crear una neva tarea linkeada al usuario

--- task/admin.py
+++ task/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Task
+
+
+class TaskAdmin(admin.ModelAdmin):
+    readonly_fields = ('date_created_at', 'updated_at')
+
 
 # Register your models here.
+admin.site.register(Task, TaskAdmin)
