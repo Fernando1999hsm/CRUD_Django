6db071b18a01fb5299645883bffc5a46dc745b3d
Nando <fernandocalva15@gmail.com>
2026-07-07 01:44:30 -0600

    Agrego login required para evitar entrar a paginas sin estar logeado, Fix a bug de etiqueta Created on: y fix footer para abarcar toda la pagina

--- task/views.py
+++ task/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.db import IntegrityError
 from django.utils import timezone
@@ -37,12 +38,14 @@ def signup(request):
                 'form': UserCreationForm(),
                 "error": "Contraseñas no coinciden"
             })
-        
+
+@login_required        
 def task(request):
     tasks = Task.objects.filter(user=request.user, date_completed__isnull=True)
     tasks_completed = Task.objects.filter(user=request.user, date_completed__isnull=False).order_by('-date_completed')
     return render(request, 'task.html', {'tasks': tasks, 'tasks_completed': tasks_completed})
 
+@login_required
 def create_task(request):
     if request.method == 'GET':
         return render(request, 'create_task.html', {
@@ -85,6 +88,7 @@ def signin(request):
             login(request, user)
             return redirect('task')
 
+@login_required
 def task_detail(request, task_id):
     task = get_object_or_404(Task, pk=task_id, user=request.user)
     if request.method == 'POST':
@@ -100,6 +104,7 @@ def task_detail(request, task_id):
         form = TaskForm(instance=task)
     return render(request, 'task_detail.html', {'task': task, 'form': form})
 
+@login_required
 def complete_task(request, task_id):
     task = get_object_or_404(Task, pk=task_id, user=request.user)
     if request.method == 'POST':
@@ -110,6 +115,7 @@ def complete_task(request, task_id):
         task.save()
     return redirect('task')
 
+@login_required
 def delete_task(request, task_id):
     task = get_object_or_404(Task, pk=task_id, user=request.user)
     if request.method == 'POST':
